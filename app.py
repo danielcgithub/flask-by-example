f594cd40ad86fde11aa47fc79abaddcca7311ab6
Daniel <d.cunniffe.ie@gmail.com>
2017-10-07 21:29:57 +0100

    Setting up a PostgreSQL database along with SQLAlchemy and Alembic to handle migrations

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')

4f1bfe3a8504f423de8a334fde03f5f64651bd9a
Daniel <d.cunniffe.ie@gmail.com>
2017-10-07 18:15:00 +0100

    Updating to have dynamic environment configuration

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
